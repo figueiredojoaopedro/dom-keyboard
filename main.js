d5bb5d9fd8db8bb18e3b916bfde253a19b748f32
figueiredojoaopedro <neewprojecti@gmail.com>
2021-12-30 16:59:50 -0300

    eletronic keyboard done

--- main.js
+++ main.js
@@ -4,15 +4,44 @@ const keys = document.querySelectorAll(".key")
 console.log(keys)
 // play notes
 function playNote(event){
-    console.log(event)
+
+    console.log(event.keyCode)
     // get key code
-
+    let audioKeyCode = getKeyCode(event)
     // typed or pressed key
-
+    const key = document.querySelector(`[data-key="${audioKeyCode}"]`)
+    console.log(key)
     // if key exists
+    const cantFoundAnyKey = !key
 
+    if(cantFoundAnyKey){
+        return
+    }
     // play audio
+    playAudio(audioKeyCode)
 }
+
+function playAudio(audioKeyCode){
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime = 0
+    audio.play()
+}
+
+function getKeyCode(event){
+    let keyCode;
+    
+    const isKeyboard = event.type === "keydown"
+    if(isKeyboard)
+    {
+        keyCode = event.keyCode
+    }
+    else
+    {
+        keyCode = event.target.dataset.key
+    }
+    return keyCode
+}
+
 // click with mouse
 keys.forEach(function(key){
     key.addEventListener("click", playNote)
